4f3ae5d9c8efb736be521d1af20e2b89d17fb5df
Fabien RENAUD <frd.frenaud@gmail.com>
2019-10-10 10:43:47 +0200

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const app           = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    next();
+});
+
+app.get('/messageepsi', (req, res) => { res.send("hello") });
+app.listen(3000);
